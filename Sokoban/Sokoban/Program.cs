5b07ccaedbc43c06364380849577b6e1aaefcc84
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-06-05 16:10:11 +0200

    Make scaffolding of engine to run the game.

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Geisha.Engine.Windows;
 
 namespace Sokoban
 {
@@ -10,6 +11,7 @@ namespace Sokoban
         [STAThread]
         private static void Main()
         {
+            WindowsApplication.Run(new SokobanGame());
         }
     }
 }
---
+++ Sokoban/Sokoban/SokobanGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Geisha.Engine;
+
+namespace Sokoban
+{
+    internal sealed class SokobanGame : IGame
+    {
+        private static string EngineInformation =>
+            $"Geisha Engine {Assembly.GetAssembly(typeof(IGame))?.GetName().Version?.ToString(3)}";
+
+        public string WindowTitle => $"Sokoban {Assembly.GetAssembly(typeof(SokobanGame))?.GetName().Version?.ToString(2)} ({EngineInformation})";
+
+        public void RegisterComponents(IComponentsRegistry componentsRegistry)
+        {
+        }
+    }
+}
