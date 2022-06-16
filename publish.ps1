c8b4553da5f45845efd892174a95f6d376a0a0fd
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-06-16 21:51:33 +0200

    Include editor in publish script.

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,5 @@
 Remove-Item -Path .\publish -Recurse
 dotnet publish .\Sokoban\Sokoban\Sokoban.csproj --configuration Release --runtime win-x64 --output publish\bin
+dotnet publish .\Sokoban\Sokoban.Editor\Sokoban.Editor.csproj --configuration Release --runtime win-x64 --output publish\bin
 $productVersion = (Get-Item .\publish\bin\Sokoban.exe).VersionInfo.ProductVersion
 Compress-Archive -Path publish\bin\* -DestinationPath publish\Sokoban.$productVersion.zip
