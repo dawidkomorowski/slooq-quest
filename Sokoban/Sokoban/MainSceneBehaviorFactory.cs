2055293a66192a73c23e64c85e4bdb3e0c48d306
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-08-17 20:58:14 +0200

    Preload heavy assets on game startup.

--- Sokoban/Sokoban/MainSceneBehaviorFactory.cs
+++ Sokoban/Sokoban/MainSceneBehaviorFactory.cs
@@ -2,7 +2,10 @@
 using System.Drawing;
 using System.Reflection;
 using System.Windows.Forms;
+using Geisha.Engine.Animation;
+using Geisha.Engine.Core.Assets;
 using Geisha.Engine.Core.SceneModel;
+using Sokoban.Assets;
 using Sokoban.Core;
 using Sokoban.Core.SceneLoading;
 using Sokoban.VisualEffects;
@@ -13,24 +16,28 @@ namespace Sokoban
     {
         private const string SceneBehaviorName = "Main";
 
+        private readonly IAssetStore _assetStore;
         private readonly CoreEntityFactory _coreEntityFactory;
 
-        public MainSceneBehaviorFactory(CoreEntityFactory coreEntityFactory)
+        public MainSceneBehaviorFactory(CoreEntityFactory coreEntityFactory, IAssetStore assetStore)
         {
             _coreEntityFactory = coreEntityFactory;
+            _assetStore = assetStore;
         }
 
         public string BehaviorName => SceneBehaviorName;
-        public SceneBehavior Create(Scene scene) => new MainSceneBehavior(scene, _coreEntityFactory);
+        public SceneBehavior Create(Scene scene) => new MainSceneBehavior(scene, _coreEntityFactory, _assetStore);
 
         private sealed class MainSceneBehavior : SceneBehavior
         {
+            private readonly IAssetStore _assetStore;
             private readonly CoreEntityFactory _coreEntityFactory;
 
 
-            public MainSceneBehavior(Scene scene, CoreEntityFactory coreEntityFactory) : base(scene)
+            public MainSceneBehavior(Scene scene, CoreEntityFactory coreEntityFactory, IAssetStore assetStore) : base(scene)
             {
                 _coreEntityFactory = coreEntityFactory;
+                _assetStore = assetStore;
             }
 
             public override string Name => SceneBehaviorName;
@@ -38,6 +45,8 @@ namespace Sokoban
             protected override void OnLoaded()
             {
                 Application.OpenForms[0].Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+
+                PreloadHeavyAssets();
 
                 _coreEntityFactory.CreateCamera(Scene);
 
@@ -48,6 +57,11 @@ namespace Sokoban
                 var entity = Scene.CreateEntity();
                 var loadSceneComponent = entity.CreateComponent<LoadSceneComponent>();
                 loadSceneComponent.SceneBehaviorName = "MainMenu";
+            }
+
+            private void PreloadHeavyAssets()
+            {
+                _assetStore.GetAsset<SpriteAnimation>(SokobanAssetId.Animations.Smoke.Default);
             }
         }
     }
