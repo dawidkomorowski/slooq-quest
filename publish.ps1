c3cbae1703db77565844aebc6e6d8de795982749
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-08-28 19:00:19 +0200

    Include license information in published package.

--- publish.ps1
+++ publish.ps1
@@ -3,5 +3,8 @@ $ErrorActionPreference = "Stop"
 Remove-Item -Path .\publish -Recurse
 dotnet publish .\Sokoban\Sokoban\Sokoban.csproj --configuration Release --runtime win-x64 --output publish\bin
 dotnet publish .\Sokoban\Sokoban.Editor\Sokoban.Editor.csproj --configuration Release --runtime win-x64 --output publish\bin
+Remove-Item -Path .\publish\bin\*.pdb
+Copy-Item -Path .\LICENSE -Destination .\publish\bin\LICENSE.txt
+Copy-Item -Path .\ThirdPartyNotices.txt -Destination .\publish\bin\ThirdPartyNotices.txt
 $productVersion = (Get-Item .\publish\bin\Sokoban.exe).VersionInfo.ProductVersion
 Compress-Archive -Path publish\bin\* -DestinationPath publish\Sokoban.$productVersion.zip
